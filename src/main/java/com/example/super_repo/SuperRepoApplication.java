1760be0cb2f19402b30d0a0cc0373e8bde28f638
oooo91 <hyelneu29@gmail.com>
2023-02-01 21:04:09 +0900

    super_test

---
+++ src/main/java/com/example/super_repo/SuperRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.super_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperRepoApplication.class, args);
+    }
+
+}
